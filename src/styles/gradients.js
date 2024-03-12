c44c93dd652c52d89a7d21911db95b2b426e76ea
joacomesa22 <joacomesa@gmail.com>
2024-03-12 11:24:35 -0300

    card

---
+++ src/styles/gradients.js
@@ -0,0 +1,8 @@
+export const blueGradient =
+  "bg-[linear-gradient(to_right_top,_#003566,_#002f5b,_#002951,_#002347,_#001d3d)]";
+
+export const yellowGradient =
+  "bg-[linear-gradient(to_right_top,_#ffd60a,_#ffd106,_#ffcc03,_#ffc801,_#ffc300)]";
+
+export const grayGradient =
+  "bg-[linear-gradient(to_right_top,_#f2f2f2,_#efefef,_#ececec,_#eaeaea,_#e7e7e7)]";
