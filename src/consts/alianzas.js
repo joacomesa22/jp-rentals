2d21e9fbb2725e4d153317be1cdcf9e77fb235ba
joacomesa22 <joacomesa@gmail.com>
2024-03-25 20:04:42 -0300

    contact

---
+++ src/consts/alianzas.js
@@ -0,0 +1,14 @@
+export const ALIANZAS = [
+  {
+    name: "Airbnb",
+    img: "./assets/img/airbnb.png",
+  },
+  {
+    name: "Booking",
+    img: "./assets/img/booking.png",
+  },
+  {
+    name: "Airbnb",
+    img: "./assets/img/airbnb.png",
+  },
+];
